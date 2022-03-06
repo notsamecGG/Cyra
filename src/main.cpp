63a8ad334ae0c0f517ccac8947d60dd1a1db22fd
Sam-main <samuel.grulich@gmail.com>
2022-03-06 17:31:57 +0100

    glfw docs

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,55 @@
+#include <stdio.h>
+
+#define GLFW_INCLUDE_NONE // tell glfw not to include its opengl 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
+static void glfw_error_callback(int error, const char* desc)
+{
+    fprintf(stderr, "glfw error: %s\n", desc);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+
 int main(void)
 {
+    // setup
     GLFWwindow* window;
 
-    /* Initialize the library */
     if (!glfwInit())
-        return -1;
+        return -1; // Initialization failed
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    glfwSetErrorCallback(glfw_error_callback);
+
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2); // opengl version
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    window = glfwCreateWindow(600, 600, "Title", NULL, NULL);
+
     if (!window)
+        return -1; // window or opengl context creation failed
+
+    glfwMakeContextCurrent(window); // make opengl context current
+
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress); // link with glad
+
+    glfwSetKeyCallback(window, key_callback);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) // glfwSetWindowCloseCallbakc, glfwSetWindowShouldClose
     {
-        glfwTerminate();
-        return -1;
+
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    // cleanup
+    glfwDestroyWindow(window);
 
-    /**/
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwTerminate(); // destroy remaining windows
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
     return 0;
 }
