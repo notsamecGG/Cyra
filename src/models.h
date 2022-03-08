cca8e7a1e14d31f4e7a567ff7b502b56e95fc614
Sam-gglmain <samuel.grulich@gmail.com>
2022-03-08 22:58:53 +0100

    square with DrawElems

--- src/models.h
+++ src/models.h
@@ -1,12 +1,10 @@
-float verticies[] = 
-{
-    // bottom left half
-    -0.5f, -0.5f,  0.0f,
-     0.5f, -0.5f,  0.0f,
-    -0.5f,  0.5f,  0.0f,
-
-    // top right half
-     0.5f, -0.5f,  0.0f,
-     0.5f,  0.5f,  0.0f,
-    -0.5f,  0.5f,  0.0f
+float vertices[] = {
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
 };
+unsigned int indices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+}; 
