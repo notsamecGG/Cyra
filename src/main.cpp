4542753fe81c3bcdadf3d484b803577b9239f798
Sam-gglmain <samuel.grulich@gmail.com>
2022-03-09 11:15:00 +0100

    Models cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #define GLFW_INCLUDE_NONE // tell glfw not to include its opengl 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 #include "fileman.h"
 #include "models.h"
@@ -31,7 +32,7 @@ WinSize winSize(600, 600);
 int main(void)
 {
     // setup
-    // glfw setup
+#pragma region GLFW Setup
     if (!glfwInit())
         return -1; // Initialization failed
 
@@ -56,8 +57,9 @@ int main(void)
     glfwSetKeyCallback(window, key_callback); // input bind
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
+#pragma endregion
 
-    // shader setup
+#pragma region Shader Setup
     unsigned int vertexShader;
     unsigned int fragmentShader;
 
@@ -121,6 +123,7 @@ int main(void)
             printf("Shader linking error: %s \n", infoLog);
         }
     };
+#pragma endregion
 
     // buffer setup
     unsigned int VAO;
@@ -136,10 +139,10 @@ int main(void)
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Triangle::vertices), Triangle::vertices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Triangle::indices), Triangle::indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -162,7 +165,7 @@ int main(void)
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
 
--- src/models.h
+++ src/models.h
@@ -1,34 +1,53 @@
-// square
-// float vertices[] = {
-//      0.5f,  0.5f, 0.0f,  // top right
-//      0.5f, -0.5f, 0.0f,  // bottom right
-//     -0.5f, -0.5f, 0.0f,  // bottom left
-//     -0.5f,  0.5f, 0.0f   // top left 
-// };
-// unsigned int indices[] = {  // note that we start from 0!
-//     0, 1, 3,   // first triangle
-//     1, 2, 3    // second triangle
-// }; 
-
-// star
-float vertices[] = {
-     0.32f,  0.32f, 0.0f,  // top right
-     0.32f, -0.32f, 0.0f,  // bottom right
-    -0.32f, -0.32f, 0.0f,  // bottom left
-    -0.32f,  0.32f, 0.0f,  // top left
-
-     0.0f,  1.0f, 0.0f,
-     1.0f,  0.0f, 0.0f, 
-     0.0f, -1.0f, 0.0f,
-    -1.0f,  0.0f, 0.0f
+struct Triangle 
+{
+    static constexpr float vertices[] = {
+        -0.5f, -0.5f,  0.0f,
+         0.5f, -0.5f,  0.0f,
+         0.0f,  0.5f,  0.0f
+    };
+    
+    static constexpr unsigned int indices[] = {
+        0, 1, 2
+    };
 };
-unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 3,   // first triangle
-    1, 2, 3,    // second triangle
 
-    0, 3, 4,
-    1, 0, 5,
-    2, 1, 6,
-    3, 2, 7
 
-}; 
+struct Square 
+{ 
+    static constexpr float s_vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+
+    static constexpr unsigned int s_indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+};
+
+struct Star 
+{ 
+    static constexpr float st_vertices[] = {
+        0.32f,  0.32f, 0.0f,  // top right
+        0.32f, -0.32f, 0.0f,  // bottom right
+        -0.32f, -0.32f, 0.0f,  // bottom left
+        -0.32f,  0.32f, 0.0f,  // top left
+
+        0.0f,  1.0f, 0.0f,
+        1.0f,  0.0f, 0.0f, 
+        0.0f, -1.0f, 0.0f,
+        -1.0f,  0.0f, 0.0f
+    };
+
+    static constexpr unsigned int st_indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3,    // second triangle
+
+        0, 3, 4,
+        1, 0, 5,
+        2, 1, 6,
+        3, 2, 7
+    }; 
+};
